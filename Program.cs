a61fdfa905062b7adeaaa851cd63db1e560661ed
ahmadmm01 <skyahmad74@gmail.com>
2022-03-24 11:22:58 +0700

    Menambahakan method untuk koneksi ke database SQL Server

--- Program.cs
+++ Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace _20200140140_Tugas2_A
 {
     class Program
     {
+        public void SQLServer()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source= MADD\\AHMADMM; database=Kontrak_Tenant; User ID= sa; Password=root");
+                con.Open();
+
+                Console.WriteLine("Hooray, berhasil!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, sepertinya ada yang salah. " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
         static void Main(string[] args)
         {
+            new Program().SQLServer();
         }
     }
 }
