5391ab81ed64d1db065a9e4830880e96481297bb
ahmadmm01 <skyahmad74@gmail.com>
2022-03-24 11:19:51 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200140140_Tugas2_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
